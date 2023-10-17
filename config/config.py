dad850771a22c8dc03d078fe63ab5aead0da62df
BiellR7 <81019771+BiellR7@users.noreply.github.com>
2023-10-17 18:37:04 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -149,7 +149,7 @@ autoclean = []
 
 # Images
 START_IMG_URL = getenv("START_IMG_URL",
-                       "https://64.media.tumblr.com/b20a9df719f8420ac7aa02ece2cb1774/5f8ef1a042cf9e6b-7f/s540x810/eba21bbdb525e72f84be27d439c156b4dfa6b31a.gifv")
+                       "https://media.tenor.com/LWfAAmi64nwAAAAd/shinobu-kocho.gifv")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
