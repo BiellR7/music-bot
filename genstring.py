add9cef70d8ca13928fab2dfcfc9b648c2d13e8f
BiellR7 <81019771+BiellR7@users.noreply.github.com>
2023-11-13 02:45:46 -0300

    Add files via upload

--- genstring.py
+++ genstring.py
@@ -2,10 +2,10 @@ import asyncio
 
 from pyrogram import Client as c
 
-API_ID = input("\nEnter Your API_ID:\n > ")
-API_HASH = input("\nEnter Your API_HASH:\n > ")
+API_ID = input("\n➜ Coloque seu API_ID: 🔑\n➜ ")
+API_HASH = input("\n➜ Coloque seu API_HASH: 🔒\n➜ ")
 
-print("\n\n Enter Phone number when asked.\n\n")
+print("\n\n➜ O numero de telefone 📞\n\n")
 
 i = c("WinxString", in_memory=True, api_id=API_ID, api_hash=API_HASH)
 
@@ -13,7 +13,7 @@ i = c("WinxString", in_memory=True, api_id=API_ID, api_hash=API_HASH)
 async def main():
     await i.start()
     ss = await i.export_session_string()
-    print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n")
+    print("\nSua session 📋❌\n")
     print(f"\n{ss}\n")
 
 
